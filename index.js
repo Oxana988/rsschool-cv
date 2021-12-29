b6be36adcf86b65c040ec635baa5007ef42a083c
Oxana Savianok <Oxana8833@gmail.com>
2021-12-29 14:12:15 +0300

    feat: add js for burger-menu

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Burger header
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header-nav');
+    const menuCloseItem = document.querySelector('.menu-close');
+    const menuSnapItem = document.querySelector('.header-link');
+
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header-nav-active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
+    menuSnapItem.addEventListener('click', () => {
+        menu.classList.remove('header-nav-active');
+    });
+}());
